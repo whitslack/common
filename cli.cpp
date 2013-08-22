1405a68612291d21c1b98d00750af2854a15c03c
Matt Whitlock <matt@whitlock.name>
2013-08-22 16:12:27 -0400

    cli::NumericOption class template can take any numeric type as argument

--- cli.cpp
+++ cli.cpp
@@ -2,119 +2,128 @@
 
 #include <cstring>
 
-namespace CLI {
+namespace cli {
 
-	std::string OptionException::make_what(const char msg[], const Option &opt) {
-		std::string what(msg);
-		if (opt.long_form) {
-			what.append(": --").append(opt.long_form);
-		}
-		else {
-			what.append(": -") += opt.short_form;
-		}
-		return what;
+
+static std::string make_what(const char msg[], const Option &opt) {
+	std::string what(msg);
+	if (opt.long_form) {
+		(what += ": --") += opt.long_form;
 	}
-
-	bool BooleanOption::parse(char []) {
-		value = true;
-		return false;
+	else {
+		(what += ": -") += opt.short_form;
 	}
+	return what;
+}
 
-	bool StringOption::parse(char value[]) {
-		if (value) {
-			this->value = value;
+OptionException::OptionException(const char msg[], const Option &opt) : runtime_error(make_what(msg, opt)) {
+}
+
+
+bool BooleanOption::parse(char []) {
+	value = true;
+	return false;
+}
+
+
+bool StringOption::parse(char value[]) {
+	if (value) {
+		this->value = value;
+		return true;
+	}
+	throw OptionException("expected argument for option", *this);
+}
+
+
+bool StringVectorOption::parse(char value[]) {
+	if (value) {
+		if (delim == '\0') {
+			values.push_back(value);
 			return true;
 		}
-		throw CLI::OptionException("expected argument for option", *this);
-	}
-
-	bool StringVectorOption::parse(char value[]) {
-		if (value) {
-			if (delim == '\0') {
+		for (;;) {
+			char *d = std::strchr(value, delim);
+			if (!d) {
 				values.push_back(value);
 				return true;
 			}
-			for (;;) {
-				char *d = std::strchr(value, delim);
-				if (!d) {
-					values.push_back(value);
-					return true;
-				}
-				*d = '\0';
-				values.push_back(value);
-				value = d + 1;
-			}
+			*d = '\0';
+			values.push_back(value);
+			value = d + 1;
 		}
-		throw CLI::OptionException("expected argument for option", *this);
 	}
-
-	int parse(int argc, char *argv[], Option * const opts[], size_t numopts) {
-		int unused = 0;
-		for (int i = 0; i < argc; ++i) {
-			char *arg = argv[i];
-			if (arg[0] == '-') {
-				if (arg[1] == '-') {
-					if (arg[2] == '\0') {
-						while (++i < argc) {
-							argv[unused++] = argv[i];
-						}
-						return unused;
-					}
-					else {
-						arg += 2;
-						size_t optlen = std::strcspn(arg, "=");
-						for (size_t j = 0; j < numopts; ++j) {
-							const char *long_form = opts[j]->long_form;
-							if (long_form && std::strncmp(long_form, arg, optlen) == 0 && long_form[optlen] == '\0') {
-								if (arg[optlen] == '\0') {
-									if (i + 1 < argc) {
-										if (opts[j]->parse(argv[i + 1])) {
-											++i;
-										}
-									}
-									else if (opts[j]->parse(nullptr)) {
-										throw std::runtime_error("internal error while parsing command line");
-									}
-								}
-								else if (!opts[j]->parse(arg + optlen + 1)) {
-									throw CLI::OptionException("option takes no argument", *opts[j]);
-								}
-								goto next_arg;
-							}
-						}
-						throw CLI::OptionException(std::string("unrecognized option: -") + (arg - 1));
-					}
-				}
-				else if (arg[1] != '\0') {
-					for (char *flag = arg + 1; *flag != '\0'; ++flag) {
-						for (size_t j = 0; j < numopts; ++j) {
-							if (opts[j]->short_form == *flag) {
-								if (flag[1] == '\0') {
-									if (i + 1 < argc) {
-										if (opts[j]->parse(argv[i + 1])) {
-											++i;
-										}
-									}
-									else if (opts[j]->parse(nullptr)) {
-										throw std::runtime_error("internal error while parsing command line");
-									}
-								}
-								else if (!opts[j]->parse(flag + 1)) {
-									goto next_flag;
-								}
-								goto next_arg;
-							}
-						}
-						throw CLI::OptionException(std::string("unrecognized option: -") + *flag);
-					next_flag:;
-					}
-					goto next_arg;
-				}
-			}
-			argv[unused++] = argv[i];
-		next_arg:;
-		}
-		return unused;
-	}
-
+	throw OptionException("expected argument for option", *this);
 }
+
+
+int parse(int argc, char *argv[], Option * const opts[], size_t numopts) {
+	int unused = 0;
+	for (int i = 0; i < argc; ++i) {
+		char *arg = argv[i];
+		if (arg[0] == '-') {
+			if (arg[1] == '-') {
+				if (arg[2] == '\0') {
+					while (++i < argc) {
+						argv[unused++] = argv[i];
+					}
+					return unused;
+				}
+				else {
+					arg += 2;
+					size_t optlen = std::strcspn(arg, "=");
+					for (size_t j = 0; j < numopts; ++j) {
+						const char *long_form = opts[j]->long_form;
+						if (long_form && std::strncmp(long_form, arg, optlen) == 0 && long_form[optlen] == '\0') {
+							if (arg[optlen] == '\0') {
+								if (i + 1 < argc) {
+									if (opts[j]->parse(argv[i + 1])) {
+										++i;
+									}
+								}
+								else if (opts[j]->parse(nullptr)) {
+									throw std::runtime_error("internal error while parsing command line");
+								}
+							}
+							else if (!opts[j]->parse(arg + optlen + 1)) {
+								throw OptionException("option takes no argument", *opts[j]);
+							}
+							goto next_arg;
+						}
+					}
+					throw OptionException(std::string("unrecognized option: -") + (arg - 1));
+				}
+			}
+			else if (arg[1] != '\0') {
+				for (char *flag = arg + 1; *flag != '\0'; ++flag) {
+					for (size_t j = 0; j < numopts; ++j) {
+						if (opts[j]->short_form == *flag) {
+							if (flag[1] == '\0') {
+								if (i + 1 < argc) {
+									if (opts[j]->parse(argv[i + 1])) {
+										++i;
+									}
+								}
+								else if (opts[j]->parse(nullptr)) {
+									throw std::runtime_error("internal error while parsing command line");
+								}
+							}
+							else if (!opts[j]->parse(flag + 1)) {
+								goto next_flag;
+							}
+							goto next_arg;
+						}
+					}
+					throw OptionException(std::string("unrecognized option: -") + *flag);
+				next_flag:;
+				}
+				goto next_arg;
+			}
+		}
+		argv[unused++] = argv[i];
+	next_arg:;
+	}
+	return unused;
+}
+
+
+} // namespace cli
