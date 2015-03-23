90b995fab18549eeb480bac850cbd68556e896ea
Matt Whitlock <matt@whitlock.name>
2015-03-23 17:42:35 -0400

    implement connection timeout

--- connect.cpp
+++ connect.cpp
@@ -1,6 +1,5 @@
 #include "connect.h"
 
-#include <chrono>
 #include <thread>
 
 #include "dns.h"
@@ -8,7 +7,8 @@
 
 extern Log elog;
 
-Socket connect(const char host[], uint16_t port, int send_buffer_size) {
+
+Socket connect(const char host[], uint16_t port, std::chrono::microseconds timeout) {
 	for (auto &info : getaddrinfo(host)) {
 		if (info.ai_family == AF_INET) {
 			reinterpret_cast<sockaddr_in *>(info.ai_addr)->sin_port = htobe16(port);
@@ -29,25 +29,34 @@ Socket connect(const char host[], uint16_t port, int send_buffer_size) {
 			Socket socket(info.ai_family, info.ai_socktype | SOCK_CLOEXEC, info.ai_protocol);
 			int optval = 1;
 			socket.setsockopt(SOL_SOCKET, SO_KEEPALIVE, &optval, static_cast<socklen_t>(sizeof optval));
-			if (send_buffer_size >= 0) {
-				socket.setsockopt(SOL_SOCKET, SO_SNDBUF, &send_buffer_size, static_cast<socklen_t>(sizeof send_buffer_size));
+			if (timeout > std::chrono::microseconds::zero()) {
+				struct timeval tv;
+				tv.tv_sec = static_cast<decltype(tv.tv_sec)>(std::chrono::duration_cast<std::chrono::seconds>(timeout).count());
+				tv.tv_usec = static_cast<decltype(tv.tv_usec)>((timeout % std::chrono::seconds(1)).count());
+				socket.setsockopt(SOL_SOCKET, SO_SNDTIMEO, &tv, static_cast<socklen_t>(sizeof tv));
 			}
-			socket.connect(info.ai_addr, info.ai_addrlen);
-			if (elog.debug_enabled()) {
-				if (info.ai_family == AF_INET) {
-					sockaddr_in addr;
-					socklen_t addrlen = static_cast<socklen_t>(sizeof addr);
-					socket.getpeername(reinterpret_cast<sockaddr *>(&addr), &addrlen);
-					elog.debug() << "connected to " << host << " at " << addr << std::endl;
+			if (socket.connect(info.ai_addr, info.ai_addrlen)) {
+				if (timeout > std::chrono::microseconds::zero()) {
+					struct timeval tv = { 0, 0 };
+					socket.setsockopt(SOL_SOCKET, SO_SNDTIMEO, &tv, static_cast<socklen_t>(sizeof tv));
 				}
-				else if (info.ai_family == AF_INET6) {
-					sockaddr_in6 addr;
-					socklen_t addrlen = static_cast<socklen_t>(sizeof addr);
-					socket.getpeername(reinterpret_cast<sockaddr *>(&addr), &addrlen);
-					elog.debug() << "connected to " << host << " at " << addr << std::endl;
+				if (elog.debug_enabled()) {
+					if (info.ai_family == AF_INET) {
+						sockaddr_in addr;
+						socklen_t addrlen = static_cast<socklen_t>(sizeof addr);
+						socket.getpeername(reinterpret_cast<sockaddr *>(&addr), &addrlen);
+						elog.debug() << "connected to " << host << " at " << addr << std::endl;
+					}
+					else if (info.ai_family == AF_INET6) {
+						sockaddr_in6 addr;
+						socklen_t addrlen = static_cast<socklen_t>(sizeof addr);
+						socket.getpeername(reinterpret_cast<sockaddr *>(&addr), &addrlen);
+						elog.debug() << "connected to " << host << " at " << addr << std::endl;
+					}
 				}
+				return socket;
 			}
-			return socket;
+			errno = ETIMEDOUT;
 		}
 		catch (...) {
 			continue;
@@ -56,14 +65,14 @@ Socket connect(const char host[], uint16_t port, int send_buffer_size) {
 	throw std::system_error(errno, std::system_category(), "connect");
 }
 
-Socket connect_with_retry(const char host[], uint16_t port, int send_buffer_size) {
+Socket connect_with_retry(const char host[], uint16_t port) {
 	static constexpr std::chrono::steady_clock::duration
 			min_delay = std::chrono::milliseconds(500),
 			max_delay = std::chrono::seconds(15);
 	auto reconnect_delay = min_delay;
 	for (;;) {
 		try {
-			return connect(host, port, send_buffer_size);
+			return connect(host, port, std::chrono::duration_cast<std::chrono::microseconds>(reconnect_delay));
 		}
 		catch (const std::exception &e) {
 			if (elog.warn_enabled()) {
--- connect.h
+++ connect.h
@@ -1,5 +1,8 @@
+#include <chrono>
+
 #include "socket.h"
 
-Socket connect(const char host[], uint16_t port, int send_buffer_size = -1);
 
-Socket connect_with_retry(const char host[], uint16_t port, int send_buffer_size = -1);
+Socket connect(const char host[], uint16_t port, std::chrono::microseconds timeout = std::chrono::microseconds::zero());
+
+Socket connect_with_retry(const char host[], uint16_t port);
