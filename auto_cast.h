eb027cba92176d00910aa001d2be9b5e1f87fe9d
Matt Whitlock <matt@whitlock.name>
2015-12-31 02:35:36 -0500

    add auto_cast by GManNickG

---
+++ auto_cast.h
@@ -0,0 +1,40 @@
+// copied from GManNickG's blog post at http://www.gmannickg.com/?p=28
+
+#include <utility>
+
+template <typename T>
+class auto_cast_wrapper
+{
+public:
+	template <typename R>
+	friend auto_cast_wrapper<R> auto_cast(R&& x);
+
+	template <typename U>
+	operator U() const
+	{
+		// doesn't allow downcasts, otherwise acts like static_cast
+		// see: http://stackoverflow.com/questions/5693432/making-auto-cast-safe
+		//return U{std::forward<T>(mX)};
+		// [MLW] The above emits a warning on narrowing conversions.
+		return static_cast<U>(std::forward<T>(mX));
+	}
+
+private:
+	auto_cast_wrapper(T&& x) :
+	mX(std::forward<T>(x))
+	{}
+
+	auto_cast_wrapper(const auto_cast_wrapper& other) :
+	mX(std::forward<T>(other.mX))
+	{}
+
+	auto_cast_wrapper& operator=(const auto_cast_wrapper&) = delete;
+
+	T&& mX;
+};
+
+template <typename R>
+auto_cast_wrapper<R> auto_cast(R&& x)
+{
+	return auto_cast_wrapper<R>(std::forward<R>(x));
+}
