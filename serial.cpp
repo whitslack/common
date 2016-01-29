whitslack/common

commit b0bfa02713bc83b303ab1cf13277b8c5446877c4

modernize endianness conversions using decorator types

serial.cpp
@@ -34,23 +34,23 @@ Source & _weak read_varint(Source &source, int32_t &value) {
 		}
 		case 3: {
 			int32_t x = b << 27;
-			uint16_t w;
+			be<uint16_t> w;
 			source.read_fully(&w, sizeof w);
-			value = (x >> 11 | be16toh(w)) + (x >> 31 | 1) * 0x2040;
+			value = (x >> 11 | w) + (x >> 31 | 1) * 0x2040;
 			break;
 		}
 		case 4: {
 			int32_t x = b << 28;
-			uint32_t d = 0;
+			be<uint32_t> d = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
-			value = (x >> 4 | be32toh(d)) + (x >> 31 | 1) * 0x102040;
+			value = (x >> 4 | d) + (x >> 31 | 1) * 0x102040;
 			break;
 		}
 		case 5: {
 			int32_t x = b << 29;
 			if (static_cast<uint32_t>((x >> 29) + 1) <= 1) {
 				source.read_fully(&value, sizeof value);
-				value = be32toh(value);
+				value = as_be(value);
 				if (x < 0 ? (value -= 0x08102040) < 0 : (value += 0x08102040) >= 0) {
 					break;
 				}
@@ -69,19 +69,19 @@ Sink & _weak write_varint(Sink &sink, int32_t value) {
 			sink.write_fully(&b, sizeof b);
 		}
 		else if ((value += (1 << 6)) > ~(1 << 13)) {
-			uint16_t w = htobe16(static_cast<uint16_t>(~(1 << 14) & value));
+			be<uint16_t> w = static_cast<uint16_t>(~(1 << 14) & value);
 			sink.write_fully(&w, sizeof w);
 		}
 		else if ((value += (1 << 13)) > ~(1 << 20)) {
-			uint32_t d = htobe32(~(1 << 21) & value);
+			be<uint32_t> d = ~(1 << 21) & value;
 			sink.write_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
 		}
 		else if ((value += (1 << 20)) > ~(1 << 27)) {
-			uint32_t d = htobe32(~(1 << 28) & value);
+			be<uint32_t> d = ~(1 << 28) & value;
 			sink.write_fully(&d, sizeof d);
 		}
 		else {
-			uint32_t a[2] = { htobe32(0xF7), htobe32(value + (1 << 27)) };
+			be<uint32_t> a[2] = { 0xF7, value + (1 << 27) };
 			sink.write_fully(reinterpret_cast<uint8_t *>(a) + 3, sizeof a - 3);
 		}
 	}
@@ -91,19 +91,19 @@ Sink & _weak write_varint(Sink &sink, int32_t value) {
 			sink.write_fully(&b, sizeof b);
 		}
 		else if ((value -= (1 << 6)) < (1 << 13)) {
-			uint16_t w = htobe16(static_cast<uint16_t>(0x8000 | value));
+			be<uint16_t> w = static_cast<uint16_t>(0x8000 | value);
 			sink.write_fully(&w, sizeof w);
 		}
 		else if ((value -= (1 << 13)) < (1 << 20)) {
-			uint32_t d = htobe32(0xC00000 | value);
+			be<uint32_t> d = 0xC00000 | value;
 			sink.write_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
 		}
 		else if ((value -= (1 << 20)) < (1 << 27)) {
-			uint32_t d = htobe32(0xE0000000 | value);
+			be<uint32_t> d = 0xE0000000 | value;
 			sink.write_fully(&d, sizeof d);
 		}
 		else {
-			uint32_t a[2] = { htobe32(0xF0), htobe32(value - (1 << 27)) };
+			be<uint32_t> a[2] = { 0xF0, value - (1 << 27) };
 			sink.write_fully(reinterpret_cast<uint8_t *>(a) + 3, sizeof a - 3);
 		}
 	}
@@ -124,21 +124,21 @@ Source & _weak read_varint(Source &source, uint32_t &value) {
 			break;
 		}
 		case 3: {
-			uint16_t w;
+			be<uint16_t> w;
 			source.read_fully(&w, sizeof w);
-			value = ((b & 0x1F) << 16 | be16toh(w)) + 0x4080;
+			value = ((b & 0x1F) << 16 | w) + 0x4080;
 			break;
 		}
 		case 4: {
-			uint32_t d = 0;
+			be<uint32_t> d = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
-			value = ((b & 0x0F) << 24 | be32toh(d)) + 0x204080;
+			value = ((b & 0x0F) << 24 | d) + 0x204080;
 			break;
 		}
 		case 5: {
 			if ((b & 0x07) == 0) {
 				source.read_fully(&value, sizeof value);
-				value = be32toh(value);
+				value = as_be(value);
 				if (value <= UINT32_MAX - 0x10204080) {
 					value += 0x10204080;
 					break;
@@ -157,19 +157,19 @@ Sink & _weak write_varint(Sink &sink, uint32_t value) {
 		sink.write_fully(&b, sizeof b);
 	}
 	else if ((value -= (1 << 7)) < (1 << 14)) {
-		uint16_t w = htobe16(static_cast<uint16_t>(0x8000 | value));
+		be<uint16_t> w = static_cast<uint16_t>(0x8000 | value);
 		sink.write_fully(&w, sizeof w);
 	}
 	else if ((value -= (1 << 14)) < (1 << 21)) {
-		uint32_t d = htobe32(0xC00000 | value);
+		be<uint32_t> d = 0xC00000 | value;
 		sink.write_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
 	}
 	else if ((value -= (1 << 21)) < (1 << 28)) {
-		uint32_t d = htobe32(0xE0000000 | value);
+		be<uint32_t> d = 0xE0000000 | value;
 		sink.write_fully(&d, sizeof d);
 	}
 	else {
-		uint32_t a[2] = { htobe32(0xF0), htobe32(value - (1 << 28)) };
+		be<uint32_t> a[2] = { 0xF0, value - (1 << 28) };
 		sink.write_fully(reinterpret_cast<uint8_t *>(a) + 3, sizeof a - 3);
 	}
 	return sink;
@@ -191,49 +191,49 @@ Source & _weak read_varint(Source &source, int64_t &value) {
 		}
 		case 3: {
 			int32_t x = b << 27;
-			uint16_t w;
+			be<uint16_t> w;
 			source.read_fully(&w, sizeof w);
-			value = (x >> 11 | be16toh(w)) + (x >> 31 | 1) * 0x2040;
+			value = (x >> 11 | w) + (x >> 31 | 1) * 0x2040;
 			break;
 		}
 		case 4: {
 			int32_t x = b << 28;
-			uint32_t d = 0;
+			be<uint32_t> d = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
-			value = static_cast<int32_t>(x >> 4 | be32toh(d)) + (x >> 31 | 1) * 0x102040;
+			value = static_cast<int32_t>(x >> 4 | d) + (x >> 31 | 1) * 0x102040;
 			break;
 		}
 		case 5: {
 			int64_t x = static_cast<int64_t>(b) << 61;
-			uint32_t d;
+			be<uint32_t> d;
 			source.read_fully(&d, sizeof d);
-			value = (x >> 29 | be32toh(d)) + (x >> 63 | 1) * 0x08102040;
+			value = (x >> 29 | d) + (x >> 63 | 1) * 0x08102040;
 			break;
 		}
 		case 6: {
 			int64_t x = static_cast<int64_t>(b) << 62;
-			uint64_t q = 0;
+			be<uint64_t> q = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&q) + 3, sizeof q - 3);
-			value = (x >> 22 | be64toh(q)) + (x >> 63 | 1) * INT64_C(0x0408102040);
+			value = (x >> 22 | q) + (x >> 63 | 1) * INT64_C(0x0408102040);
 			break;
 		}
 		case 7: {
 			int64_t x = static_cast<int64_t>(b) << 63;
-			uint64_t q = 0;
+			be<uint64_t> q = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&q) + 2, sizeof q - 2);
-			value = (x >> 15 | be64toh(q)) + (x >> 63 | 1) * INT64_C(0x020408102040);
+			value = (x >> 15 | q) + (x >> 63 | 1) * INT64_C(0x020408102040);
 			break;
 		}
 		case 8: {
 			int64_t x;
 			source.read_fully(reinterpret_cast<uint8_t *>(&x) + 1, sizeof x - 1);
-			x = be64toh(x) << 8;
+			x = as_be(x) << 8;
 			value = (x >> 8) + (x >> 63 | 1) * INT64_C(0x01020408102040);
 			break;
 		}
 		case 9:
 			source.read_fully(&value, sizeof value);
-			value = be64toh(value);
+			value = as_be(value);
 			if (value < 0 ? (value -= INT64_C(0x0081020408102040)) < 0 : (value += INT64_C(0x0081020408102040)) >= 0) {
 				break;
 			}
@@ -250,35 +250,35 @@ Sink & _weak write_varint(Sink &sink, int64_t value) {
 			sink.write_fully(&b, sizeof b);
 		}
 		else if ((value += (1 << 6)) > ~(1 << 13)) {
-			uint16_t w = htobe16(static_cast<uint16_t>(~(1 << 14) & value));
+			be<uint16_t> w = static_cast<uint16_t>(~(1 << 14) & value);
 			sink.write_fully(&w, sizeof w);
 		}
 		else if ((value += (1 << 13)) > ~(1 << 20)) {
-			uint32_t d = htobe32(static_cast<uint32_t>(~(1 << 21) & value));
+			be<uint32_t> d = static_cast<uint32_t>(~(1 << 21) & value);
 			sink.write_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
 		}
 		else if ((value += (1 << 20)) > ~(1 << 27)) {
-			uint32_t d = htobe32(static_cast<uint32_t>(~(1 << 28) & value));
+			be<uint32_t> d = static_cast<uint32_t>(~(1 << 28) & value);
 			sink.write_fully(&d, sizeof d);
 		}
 		else if ((value += (1 << 27)) > ~(INT64_C(1) << 34)) {
-			uint64_t q = htobe64(~(INT64_C(1) << 35) & value);
+			be<uint64_t> q = ~(INT64_C(1) << 35) & value;
 			sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 3, sizeof q - 3);
 		}
 		else if ((value += (INT64_C(1) << 34)) > ~(INT64_C(1) << 41)) {
-			uint64_t q = htobe64(~(INT64_C(1) << 42) & value);
+			be<uint64_t> q = ~(INT64_C(1) << 42) & value;
 			sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 2, sizeof q - 2);
 		}
 		else if ((value += (INT64_C(1) << 41)) > ~(INT64_C(1) << 48)) {
-			uint64_t q = htobe64(~(INT64_C(1) << 49) & value);
+			be<uint64_t> q = ~(INT64_C(1) << 49) & value;
 			sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 1, sizeof q - 1);
 		}
 		else if ((value += (INT64_C(1) << 48)) > ~(INT64_C(1) << 55)) {
-			uint64_t q = htobe64(~(INT64_C(1) << 56) & value);
+			be<uint64_t> q = ~(INT64_C(1) << 56) & value;
 			sink.write_fully(&q, sizeof q);
 		}
 		else {
-			uint64_t a[2] = { htobe64(0xFF), htobe64(value + (INT64_C(1) << 55)) };
+			be<uint64_t> a[2] = { 0xFF, value + (INT64_C(1) << 55) };
 			sink.write_fully(reinterpret_cast<uint8_t *>(a) + 7, sizeof a - 7);
 		}
 	}
@@ -288,35 +288,35 @@ Sink & _weak write_varint(Sink &sink, int64_t value) {
 			sink.write_fully(&b, sizeof b);
 		}
 		else if ((value -= (1 << 6)) < (1 << 13)) {
-			uint16_t w = htobe16(static_cast<uint16_t>(0x8000 | value));
+			be<uint16_t> w = static_cast<uint16_t>(0x8000 | value);
 			sink.write_fully(&w, sizeof w);
 		}
 		else if ((value -= (1 << 13)) < (1 << 20)) {
-			uint32_t d = htobe32(static_cast<uint32_t>(0xC00000 | value));
+			be<uint32_t> d = static_cast<uint32_t>(0xC00000 | value);
 			sink.write_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
 		}
 		else if ((value -= (1 << 20)) < (1 << 27)) {
-			uint32_t d = htobe32(static_cast<uint32_t>(0xE0000000 | value));
+			be<uint32_t> d = static_cast<uint32_t>(0xE0000000 | value);
 			sink.write_fully(&d, sizeof d);
 		}
 		else if ((value -= (1 << 27)) < (INT64_C(1) << 34)) {
-			uint64_t q = htobe64(INT64_C(0xF000000000) | value);
+			be<uint64_t> q = INT64_C(0xF000000000) | value;
 			sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 3, sizeof q - 3);
 		}
 		else if ((value -= (INT64_C(1) << 34)) < (INT64_C(1) << 41)) {
-			uint64_t q = htobe64(INT64_C(0xF80000000000) | value);
+			be<uint64_t> q = INT64_C(0xF80000000000) | value;
 			sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 2, sizeof q - 2);
 		}
 		else if ((value -= (INT64_C(1) << 41)) < (INT64_C(1) << 48)) {
-			uint64_t q = htobe64(INT64_C(0xFC000000000000) | value);
+			be<uint64_t> q = INT64_C(0xFC000000000000) | value;
 			sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 1, sizeof q - 1);
 		}
 		else if ((value -= (INT64_C(1) << 48)) < (INT64_C(1) << 55)) {
-			uint64_t q = htobe64(INT64_C(0xFE00000000000000) | value);
+			be<uint64_t> q = INT64_C(0xFE00000000000000) | value;
 			sink.write_fully(&q, sizeof q);
 		}
 		else {
-			uint64_t a[2] = { htobe64(0xFF), htobe64(value - (INT64_C(1) << 55)) };
+			be<uint64_t> a[2] = { 0xFF, value - (INT64_C(1) << 55) };
 			sink.write_fully(reinterpret_cast<uint8_t *>(a) + 7, sizeof a - 7);
 		}
 	}
@@ -337,44 +337,44 @@ Source & _weak read_varint(Source &source, uint64_t &value) {
 			break;
 		}
 		case 3: {
-			uint16_t w;
+			be<uint16_t> w;
 			source.read_fully(&w, sizeof w);
-			value = ((b & 0x1F) << 16 | be16toh(w)) + 0x4080;
+			value = ((b & 0x1F) << 16 | w) + 0x4080;
 			break;
 		}
 		case 4: {
-			uint32_t d = 0;
+			be<uint32_t> d = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
-			value = ((b & 0x0F) << 24 | be32toh(d)) + 0x204080;
+			value = ((b & 0x0F) << 24 | d) + 0x204080;
 			break;
 		}
 		case 5: {
-			uint32_t d;
+			be<uint32_t> d;
 			source.read_fully(&d, sizeof d);
-			value = (static_cast<uint64_t>(b & 0x07) << 32 | be32toh(d)) + 0x10204080;
+			value = (static_cast<uint64_t>(b & 0x07) << 32 | d) + 0x10204080;
 			break;
 		}
 		case 6: {
-			uint64_t q = 0;
+			be<uint64_t> q = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&q) + 3, sizeof q - 3);
-			value = (static_cast<uint64_t>(b & 0x03) << 40 | be64toh(q)) + UINT64_C(0x0810204080);
+			value = (static_cast<uint64_t>(b & 0x03) << 40 | q) + UINT64_C(0x0810204080);
 			break;
 		}
 		case 7: {
-			uint64_t q = 0;
+			be<uint64_t> q = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&q) + 2, sizeof q - 2);
-			value = (static_cast<uint64_t>(b & 0x01) << 48 | be64toh(q)) + UINT64_C(0x040810204080);
+			value = (static_cast<uint64_t>(b & 0x01) << 48 | q) + UINT64_C(0x040810204080);
 			break;
 		}
 		case 8: {
-			uint64_t q = 0;
+			be<uint64_t> q = 0;
 			source.read_fully(reinterpret_cast<uint8_t *>(&q) + 1, sizeof q - 1);
-			value = be64toh(q) + UINT64_C(0x02040810204080);
+			value = q + UINT64_C(0x02040810204080);
 			break;
 		}
 		case 9:
 			source.read_fully(&value, sizeof value);
-			value = be64toh(value);
+			value = as_be(value);
 			if (value <= UINT64_MAX - UINT64_C(0x0102040810204080)) {
 				value += UINT64_C(0x0102040810204080);
 				break;
@@ -391,35 +391,35 @@ Sink & _weak write_varint(Sink &sink, uint64_t value) {
 		sink.write_fully(&b, sizeof b);
 	}
 	else if ((value -= (1 << 7)) < (1 << 14)) {
-		uint16_t w = htobe16(static_cast<uint16_t>(0x8000 | value));
+		be<uint16_t> w = static_cast<uint16_t>(0x8000 | value);
 		sink.write_fully(&w, sizeof w);
 	}
 	else if ((value -= (1 << 14)) < (1 << 21)) {
-		uint32_t d = htobe32(0xC00000 | static_cast<uint32_t>(value));
+		be<uint32_t> d = 0xC00000 | static_cast<uint32_t>(value);
 		sink.write_fully(reinterpret_cast<uint8_t *>(&d) + 1, sizeof d - 1);
 	}
 	else if ((value -= (1 << 21)) < (1 << 28)) {
-		uint32_t d = htobe32(0xE0000000 | static_cast<uint32_t>(value));
+		be<uint32_t> d = 0xE0000000 | static_cast<uint32_t>(value);
 		sink.write_fully(&d, sizeof d);
 	}
 	else if ((value -= (1 << 28)) < (UINT64_C(1) << 35)) {
-		uint64_t q = htobe64(UINT64_C(0xF000000000) | value);
+		be<uint64_t> q = UINT64_C(0xF000000000) | value;
 		sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 3, sizeof q - 3);
 	}
 	else if ((value -= (UINT64_C(1) << 35)) < (UINT64_C(1) << 42)) {
-		uint64_t q = htobe64(UINT64_C(0xF80000000000) | value);
+		be<uint64_t> q = UINT64_C(0xF80000000000) | value;
 		sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 2, sizeof q - 2);
 	}
 	else if ((value -= (UINT64_C(1) << 42)) < (UINT64_C(1) << 49)) {
-		uint64_t q = htobe64(UINT64_C(0xFC000000000000) | value);
+		be<uint64_t> q = UINT64_C(0xFC000000000000) | value;
 		sink.write_fully(reinterpret_cast<uint8_t *>(&q) + 1, sizeof q - 1);
 	}
 	else if ((value -= (UINT64_C(1) << 49)) < (UINT64_C(1) << 56)) {
-		uint64_t q = htobe64(UINT64_C(0xFE00000000000000) | value);
+		be<uint64_t> q = UINT64_C(0xFE00000000000000) | value;
 		sink.write_fully(&q, sizeof q);
 	}
 	else {
-		uint64_t a[2] = { htobe64(0xFF), htobe64(value - (UINT64_C(1) << 56)) };
+		be<uint64_t> a[2] = { 0xFF, value - (UINT64_C(1) << 56) };
 		sink.write_fully(reinterpret_cast<uint8_t *>(a) + 7, sizeof a - 7);
 	}
 	return sink;

aes.cpp
@@ -2,9 +2,7 @@
 
 #include <cassert>
 
-#include <endian.h>
-
-#include "compiler.h"
+#include "endian.h"
 
 static const uint8_t encrypt_sbox[256] = {
 	0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
@@ -131,10 +129,10 @@ constexpr size_t AESBase<Key_Size>::sched_size;
 template <size_t Key_Size>
 size_t AESEncrypterBase<Key_Size>::process(uint8_t (&out)[16], const uint8_t in[], size_t n _unused) {
 	assert(n == 16);
-	uint32_t s0 = be32toh(reinterpret_cast<const uint32_t *>(in)[0]) ^ this->key_schedule[0];
-	uint32_t s1 = be32toh(reinterpret_cast<const uint32_t *>(in)[1]) ^ this->key_schedule[1];
-	uint32_t s2 = be32toh(reinterpret_cast<const uint32_t *>(in)[2]) ^ this->key_schedule[2];
-	uint32_t s3 = be32toh(reinterpret_cast<const uint32_t *>(in)[3]) ^ this->key_schedule[3];
+	uint32_t s0 = reinterpret_cast<const be<uint32_t> *>(in)[0] ^ this->key_schedule[0];
+	uint32_t s1 = reinterpret_cast<const be<uint32_t> *>(in)[1] ^ this->key_schedule[1];
+	uint32_t s2 = reinterpret_cast<const be<uint32_t> *>(in)[2] ^ this->key_schedule[2];
+	uint32_t s3 = reinterpret_cast<const be<uint32_t> *>(in)[3] ^ this->key_schedule[3];
 	uint32_t t0, t1, t2, t3;
 	for (size_t i = 0;;) {
 		t0 =
@@ -189,30 +187,30 @@ size_t AESEncrypterBase<Key_Size>::process(uint8_t (&out)[16], const uint8_t in[
 				rotr(encrypt_table[static_cast<uint8_t>(t2 >>  0)], 24) ^
 				this->key_schedule[i * 8 + 3];
 	}
-	reinterpret_cast<uint32_t (&)[4]>(out)[0] = htobe32(
+	reinterpret_cast<be<uint32_t> (&)[4]>(out)[0] =
 			encrypt_sbox[static_cast<uint8_t>(t0 >> 24)] << 24 ^
 			encrypt_sbox[static_cast<uint8_t>(t1 >> 16)] << 16 ^
 			encrypt_sbox[static_cast<uint8_t>(t2 >>  8)] <<  8 ^
 			encrypt_sbox[static_cast<uint8_t>(t3 >>  0)] <<  0 ^
-			this->key_schedule[AESBase<Key_Size>::sched_size - 4]);
-	reinterpret_cast<uint32_t (&)[4]>(out)[1] = htobe32(
+			this->key_schedule[AESBase<Key_Size>::sched_size - 4];
+	reinterpret_cast<be<uint32_t> (&)[4]>(out)[1] =
 			encrypt_sbox[static_cast<uint8_t>(t1 >> 24)] << 24 ^
 			encrypt_sbox[static_cast<uint8_t>(t2 >> 16)] << 16 ^
 			encrypt_sbox[static_cast<uint8_t>(t3 >>  8)] <<  8 ^
 			encrypt_sbox[static_cast<uint8_t>(t0 >>  0)] <<  0 ^
-			this->key_schedule[AESBase<Key_Size>::sched_size - 3]);
-	reinterpret_cast<uint32_t (&)[4]>(out)[2] = htobe32(
+			this->key_schedule[AESBase<Key_Size>::sched_size - 3];
+	reinterpret_cast<be<uint32_t> (&)[4]>(out)[2] =
 			encrypt_sbox[static_cast<uint8_t>(t2 >> 24)] << 24 ^
 			encrypt_sbox[static_cast<uint8_t>(t3 >> 16)] << 16 ^
 			encrypt_sbox[static_cast<uint8_t>(t0 >>  8)] <<  8 ^
 			encrypt_sbox[static_cast<uint8_t>(t1 >>  0)] <<  0 ^
-			this->key_schedule[AESBase<Key_Size>::sched_size - 2]);
-	reinterpret_cast<uint32_t (&)[4]>(out)[3] = htobe32(
+			this->key_schedule[AESBase<Key_Size>::sched_size - 2];
+	reinterpret_cast<be<uint32_t> (&)[4]>(out)[3] =
 			encrypt_sbox[static_cast<uint8_t>(t3 >> 24)] << 24 ^
 			encrypt_sbox[static_cast<uint8_t>(t0 >> 16)] << 16 ^
 			encrypt_sbox[static_cast<uint8_t>(t1 >>  8)] <<  8 ^
 			encrypt_sbox[static_cast<uint8_t>(t2 >>  0)] <<  0 ^
-			this->key_schedule[AESBase<Key_Size>::sched_size - 1]);
+			this->key_schedule[AESBase<Key_Size>::sched_size - 1];
 	return 16;
 }
 
@@ -239,10 +237,10 @@ AESDecrypterBase<Key_Size>::AESDecrypterBase(const AESEncrypterBase<Key_Size> &e
 template <size_t Key_Size>
 size_t AESDecrypterBase<Key_Size>::process(uint8_t (&out)[16], const uint8_t in[], size_t n _unused) {
 	assert(n == 16);
-	uint32_t s0 = be32toh(reinterpret_cast<const uint32_t *>(in)[0]) ^ this->key_schedule[0];
-	uint32_t s1 = be32toh(reinterpret_cast<const uint32_t *>(in)[1]) ^ this->key_schedule[1];
-	uint32_t s2 = be32toh(reinterpret_cast<const uint32_t *>(in)[2]) ^ this->key_schedule[2];
-	uint32_t s3 = be32toh(reinterpret_cast<const uint32_t *>(in)[3]) ^ this->key_schedule[3];
+	uint32_t s0 = reinterpret_cast<const be<uint32_t> *>(in)[0] ^ this->key_schedule[0];
+	uint32_t s1 = reinterpret_cast<const be<uint32_t> *>(in)[1] ^ this->key_schedule[1];
+	uint32_t s2 = reinterpret_cast<const be<uint32_t> *>(in)[2] ^ this->key_schedule[2];
+	uint32_t s3 = reinterpret_cast<const be<uint32_t> *>(in)[3] ^ this->key_schedule[3];
 	uint32_t t0, t1, t2, t3;
 	for (size_t i = 0;;) {
 		t0 =
@@ -297,37 +295,37 @@ size_t AESDecrypterBase<Key_Size>::process(uint8_t (&out)[16], const uint8_t in[
 				rotr(decrypt_table[static_cast<uint8_t>(t0 >>  0)], 24) ^
 				this->key_schedule[i * 8 + 3];
 	}
-	reinterpret_cast<uint32_t (&)[4]>(out)[0] = htobe32(
+	reinterpret_cast<be<uint32_t> (&)[4]>(out)[0] =
 			decrypt_sbox[static_cast<uint8_t>(t0 >> 24)] << 24 ^
 			decrypt_sbox[static_cast<uint8_t>(t3 >> 16)] << 16 ^
 			decrypt_sbox[static_cast<uint8_t>(t2 >>  8)] <<  8 ^
 			decrypt_sbox[static_cast<uint8_t>(t1 >>  0)] <<  0 ^
-			this->key_schedule[AESBase<Key_Size>::sched_size - 4]);
-	reinterpret_cast<uint32_t (&)[4]>(out)[1] = htobe32(
+			this->key_schedule[AESBase<Key_Size>::sched_size - 4];
+	reinterpret_cast<be<uint32_t> (&)[4]>(out)[1] =
 			decrypt_sbox[static_cast<uint8_t>(t1 >> 24)] << 24 ^
 			decrypt_sbox[static_cast<uint8_t>(t0 >> 16)] << 16 ^
 			decrypt_sbox[static_cast<uint8_t>(t3 >>  8)] <<  8 ^
 			decrypt_sbox[static_cast<uint8_t>(t2 >>  0)] <<  0 ^
-			this->key_schedule[AESBase<Key_Size>::sched_size - 3]);
-	reinterpret_cast<uint32_t (&)[4]>(out)[2] = htobe32(
+			this->key_schedule[AESBase<Key_Size>::sched_size - 3];
+	reinterpret_cast<be<uint32_t> (&)[4]>(out)[2] =
 			decrypt_sbox[static_cast<uint8_t>(t2 >> 24)] << 24 ^
 			decrypt_sbox[static_cast<uint8_t>(t1 >> 16)] << 16 ^
 			decrypt_sbox[static_cast<uint8_t>(t0 >>  8)] <<  8 ^
 			decrypt_sbox[static_cast<uint8_t>(t3 >>  0)] <<  0 ^
-			this->key_schedule[AESBase<Key_Size>::sched_size - 2]);
-	reinterpret_cast<uint32_t (&)[4]>(out)[3] = htobe32(
+			this->key_schedule[AESBase<Key_Size>::sched_size - 2];
+	reinterpret_cast<be<uint32_t> (&)[4]>(out)[3] =
 			decrypt_sbox[static_cast<uint8_t>(t3 >> 24)] << 24 ^
 			decrypt_sbox[static_cast<uint8_t>(t2 >> 16)] << 16 ^
 			decrypt_sbox[static_cast<uint8_t>(t1 >>  8)] <<  8 ^
 			decrypt_sbox[static_cast<uint8_t>(t0 >>  0)] <<  0 ^
-			this->key_schedule[AESBase<Key_Size>::sched_size - 1]);
+			this->key_schedule[AESBase<Key_Size>::sched_size - 1];
 	return 16;
 }
 
 
 AES128Encrypter::AES128Encrypter(const uint8_t (&key)[16]) {
 	for (size_t i = 0; i < 4; ++i) {
-		key_schedule[i] = be32toh(reinterpret_cast<const uint32_t (&)[4]>(key)[i]);
+		key_schedule[i] = reinterpret_cast<const be<uint32_t> (&)[4]>(key)[i];
 	}
 	for (size_t i = 0; i < 10; ++i) {
 		key_schedule[i * 4 + 4] =
@@ -346,7 +344,7 @@ AES128Encrypter::AES128Encrypter(const uint8_t (&key)[16]) {
 
 AES192Encrypter::AES192Encrypter(const uint8_t (&key)[24]) {
 	for (size_t i = 0; i < 6; ++i) {
-		key_schedule[i] = be32toh(reinterpret_cast<const uint32_t (&)[6]>(key)[i]);
+		key_schedule[i] = reinterpret_cast<const be<uint32_t> (&)[6]>(key)[i];
 	}
 	for (size_t i = 0;;) {
 		key_schedule[i * 6 + 6] =
@@ -370,7 +368,7 @@ AES192Encrypter::AES192Encrypter(const uint8_t (&key)[24]) {
 
 AES256Encrypter::AES256Encrypter(const uint8_t (&key)[32]) {
 	for (size_t i = 0; i < 8; ++i) {
-		key_schedule[i] = be32toh(reinterpret_cast<const uint32_t (&)[8]>(key)[i]);
+		key_schedule[i] = reinterpret_cast<const be<uint32_t> (&)[8]>(key)[i];
 	}
 	for (size_t i = 0;;) {
 		key_schedule[i * 8 + 8] =

connect.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 
 #include "dns.h"
+#include "endian.h"
 #include "log.h"
 
 extern Log elog;
@@ -11,13 +12,13 @@ extern Log elog;
 Socket connect(const char host[], uint16_t port, std::chrono::microseconds timeout) {
 	for (auto &info : getaddrinfo(host)) {
 		if (info.ai_family == AF_INET) {
-			reinterpret_cast<sockaddr_in *>(info.ai_addr)->sin_port = htobe16(port);
+			as_be(reinterpret_cast<sockaddr_in *>(info.ai_addr)->sin_port) = port;
 			if (elog.trace_enabled()) {
 				elog.trace() << "connecting to " << host << " at " << *reinterpret_cast<sockaddr_in *>(info.ai_addr) << std::endl;
 			}
 		}
 		else if (info.ai_family == AF_INET6) {
-			reinterpret_cast<sockaddr_in6 *>(info.ai_addr)->sin6_port = htobe16(port);
+			as_be(reinterpret_cast<sockaddr_in6 *>(info.ai_addr)->sin6_port) = port;
 			if (elog.trace_enabled()) {
 				elog.trace() << "connecting to " << host << " at " << *reinterpret_cast<sockaddr_in6 *>(info.ai_addr) << std::endl;
 			}

endian.h
@@ -2,7 +2,11 @@
 
 #include "compiler.h"
 
+#ifdef __APPLE__
+#include <machine/endian.h>
+#else
 #include <endian.h>
+#endif
 
 #ifndef BYTE_ORDER
 # error "BYTE_ORDER is not defined"

scrypt.cpp
@@ -13,7 +13,7 @@ static void salsa20_8_core(void * _restrict out, const void * _restrict in) {
 	auto outw = reinterpret_cast<uint32_t *>(out);
 	auto inw = reinterpret_cast<const uint32_t *>(in);
 	for (size_t i = 0; i < 16; ++i) {
-		outw[i] = le32toh(inw[i]);
+		outw[i] = as_le(inw[i]);
 	}
 	for (size_t i = 0; i < 4; ++i) {
 		outw[ 4] ^= rotl(outw[ 0] + outw[12],  7);	outw[ 9] ^= rotl(outw[ 5] + outw[ 1],  7);
@@ -34,7 +34,7 @@ static void salsa20_8_core(void * _restrict out, const void * _restrict in) {
 		outw[10] ^= rotl(outw[ 9] + outw[ 8], 18);	outw[15] ^= rotl(outw[14] + outw[13], 18);
 	}
 	for (size_t i = 0; i < 16; ++i) {
-		outw[i] = htole32(outw[i] + le32toh(inw[i]));
+		as_le(outw[i]) = outw[i] + as_le(inw[i]);
 	}
 }
 

sha.cpp
@@ -12,7 +12,7 @@ SHA1::SHA1() : Hash(sha1_init) {
 void SHA1::update(const uint8_t (&block)[64]) {
 	uint32_t words[80];
 	for (size_t i = 0; i < 16; ++i) {
-		words[i] = be32toh(reinterpret_cast<const uint32_t *>(block)[i]);
+		words[i] = reinterpret_cast<const be<uint32_t> (&)[16]>(block)[i];
 	}
 	for (size_t i = 16; i < 80; ++i) {
 		words[i] = rotl(words[i - 3] ^ words[i - 8] ^ words[i - 14] ^ words[i - 16], 1);
@@ -53,7 +53,7 @@ template <size_t Digest_Size>
 void SHA256Base<Digest_Size>::update(const uint8_t (&block)[64]) {
 	uint32_t words[64];
 	for (size_t i = 0; i < 16; ++i) {
-		words[i] = be32toh(reinterpret_cast<const uint32_t *>(block)[i]);
+		words[i] = reinterpret_cast<const be<uint32_t> (&)[16]>(block)[i];
 	}
 	for (size_t i = 16; i < 64; ++i) {
 		words[i] = words[i - 16] + (rotr(words[i - 15], 7) ^ rotr(words[i - 15], 18) ^ words[i - 15] >> 3) + words[i - 7] + (rotr(words[i - 2], 17) ^ rotr(words[i - 2], 19) ^ words[i - 2] >> 10);
@@ -112,7 +112,7 @@ template <size_t Digest_Size>
 void SHA512Base<Digest_Size>::update(const uint8_t (&block)[128]) {
 	uint64_t words[80];
 	for (size_t i = 0; i < 16; ++i) {
-		words[i] = be64toh(reinterpret_cast<const uint64_t *>(block)[i]);
+		words[i] = reinterpret_cast<const be<uint64_t> (&)[16]>(block)[i];
 	}
 	for (size_t i = 16; i < 80; ++i) {
 		words[i] = words[i - 16] + (rotr(words[i - 15], 1) ^ rotr(words[i - 15], 8) ^ words[i - 15] >> 7) + words[i - 7] + (rotr(words[i - 2], 19) ^ rotr(words[i - 2], 61) ^ words[i - 2] >> 6);

commit b6e59bf8525495a8655fe4b6c5046a7150efe7f2

add missing #include in serial.cpp (broken by b0bfa02)

serial.cpp
@@ -1,5 +1,7 @@
 #include "serial.h"
 
+#include "endian.h"
+
 
 static size_t varint_size(uint8_t first_byte) {
 	unsigned i = static_cast<uint8_t>(~first_byte);