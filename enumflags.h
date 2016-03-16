74c1227ae163353e4bd39d71983be858ce4e6bf2
Matt Whitlock <matt@whitlock.name>
2016-03-16 06:13:35 -0400

    avoid KDevelop parse errors in enumflags.h (constexpr implies inline)

--- enumflags.h
+++ enumflags.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#include <ostream>
 #include <type_traits>
 
 #define DEFINE_ENUM_FLAG_OPS(E) \
 	static_assert(std::is_enum<E>::value, "must be an enum type"); \
-	static inline constexpr E operator ~ (E arg) { return static_cast<E>(~static_cast<typename std::underlying_type<E>::type>(arg)); } \
-	static inline constexpr E operator & (E lhs, E rhs) { return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) & static_cast<typename std::underlying_type<E>::type>(rhs)); } \
-	static inline constexpr E operator | (E lhs, E rhs) { return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) | static_cast<typename std::underlying_type<E>::type>(rhs)); } \
-	static inline constexpr E operator ^ (E lhs, E rhs) { return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) ^ static_cast<typename std::underlying_type<E>::type>(rhs)); } \
+	static constexpr E operator ~ (E arg) { return static_cast<E>(~static_cast<typename std::underlying_type<E>::type>(arg)); } \
+	static constexpr E operator & (E lhs, E rhs) { return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) & static_cast<typename std::underlying_type<E>::type>(rhs)); } \
+	static constexpr E operator | (E lhs, E rhs) { return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) | static_cast<typename std::underlying_type<E>::type>(rhs)); } \
+	static constexpr E operator ^ (E lhs, E rhs) { return static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) ^ static_cast<typename std::underlying_type<E>::type>(rhs)); } \
 	static inline E & operator &= (E &lhs, E rhs) { return lhs = static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) & static_cast<typename std::underlying_type<E>::type>(rhs)); } \
 	static inline E & operator |= (E &lhs, E rhs) { return lhs = static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) | static_cast<typename std::underlying_type<E>::type>(rhs)); } \
 	static inline E & operator ^= (E &lhs, E rhs) { return lhs = static_cast<E>(static_cast<typename std::underlying_type<E>::type>(lhs) ^ static_cast<typename std::underlying_type<E>::type>(rhs)); } \
