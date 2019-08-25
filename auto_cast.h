ea0249cbffa7e39764d66b62dea686599066db5d
Matt Whitlock <matt@whitlock.name>
2019-08-25 16:20:09 -0400

    auto_cast: support use in compile-time constant expressions

--- auto_cast.h
+++ auto_cast.h
@@ -1,5 +1,8 @@
+#pragma once
+
 // copied from GManNickG's blog post at http://www.gmannickg.com/?p=28
 // modified to support warning-free narrowing conversions of arithmetic types
+// modified to support use in compile-time constant expressions
 
 #include <utility>
 
@@ -7,14 +10,14 @@ template <typename T>
 class auto_cast_wrapper
 {
 	template <typename R>
-	friend auto_cast_wrapper<R> auto_cast(R&& x) noexcept;
+	friend constexpr auto_cast_wrapper<R> auto_cast(R&& x) noexcept;
 
 private:
-	auto_cast_wrapper(T&& x) noexcept :
+	constexpr auto_cast_wrapper(T&& x) noexcept :
 	mX(std::forward<T>(x))
 	{}
 
-	auto_cast_wrapper(const auto_cast_wrapper& other) noexcept :
+	constexpr auto_cast_wrapper(const auto_cast_wrapper& other) noexcept :
 	mX(std::forward<T>(other.mX))
 	{}
 
@@ -23,13 +26,13 @@ private:
 	T&& mX;
 
 	template <typename U>
-	static std::enable_if_t<std::is_arithmetic_v<U>, U> do_cast(T&& x) noexcept(noexcept(static_cast<U>(std::forward<T>(x))))
+	static constexpr std::enable_if_t<std::is_arithmetic_v<U>, U> do_cast(T&& x) noexcept(noexcept(static_cast<U>(std::forward<T>(x))))
 	{
 		return static_cast<U>(std::forward<T>(x));
 	}
 
 	template <typename U>
-	static std::enable_if_t<!std::is_arithmetic_v<U>, U> do_cast(T&& x) noexcept(noexcept(U{std::forward<T>(x)}))
+	static constexpr std::enable_if_t<!std::is_arithmetic_v<U>, U> do_cast(T&& x) noexcept(noexcept(U{std::forward<T>(x)}))
 	{
 		// doesn't allow downcasts, otherwise acts like static_cast
 		// see: http://stackoverflow.com/questions/5693432/making-auto-cast-safe
@@ -38,14 +41,14 @@ private:
 
 public:
 	template <typename U>
-	operator U() const noexcept(noexcept(do_cast<U>(std::forward<T>(mX))))
+	constexpr operator U() const noexcept(noexcept(do_cast<U>(std::forward<T>(mX))))
 	{
 		return do_cast<U>(std::forward<T>(mX));
 	}
 };
 
 template <typename R>
-auto_cast_wrapper<R> auto_cast(R&& x) noexcept
+constexpr auto_cast_wrapper<R> auto_cast(R&& x) noexcept
 {
 	return auto_cast_wrapper<R>(std::forward<R>(x));
 }
