6e190afc98311a93df9534e88443be2b18cad254
Matt Whitlock <matt@whitlock.name>
2021-01-06 20:17:58 -0500

    enumflags.h: add operator! and operator+

--- enumflags.h
+++ enumflags.h
@@ -3,16 +3,21 @@
 #include <ostream>
 #include <type_traits>
 
+#include "compiler.h"
+
+
 #define DEFINE_ENUM_FLAG_OPS(E) \
 	static_assert(std::is_enum_v<E>, "must be an enum type"); \
-	static constexpr E operator ~ (E arg) noexcept { return static_cast<E>(~static_cast<std::underlying_type_t<E>>(arg)); } \
-	static constexpr E operator & (E lhs, E rhs) noexcept { return static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) & static_cast<std::underlying_type_t<E>>(rhs)); } \
-	static constexpr E operator | (E lhs, E rhs) noexcept { return static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) | static_cast<std::underlying_type_t<E>>(rhs)); } \
-	static constexpr E operator ^ (E lhs, E rhs) noexcept { return static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) ^ static_cast<std::underlying_type_t<E>>(rhs)); } \
-	static inline E & operator &= (E &lhs, E rhs) noexcept { return lhs = static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) & static_cast<std::underlying_type_t<E>>(rhs)); } \
-	static inline E & operator |= (E &lhs, E rhs) noexcept { return lhs = static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) | static_cast<std::underlying_type_t<E>>(rhs)); } \
-	static inline E & operator ^= (E &lhs, E rhs) noexcept { return lhs = static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) ^ static_cast<std::underlying_type_t<E>>(rhs)); } \
-	static inline std::ostream & operator << (std::ostream &os, E e) { \
+	static constexpr bool _const operator!(E arg) noexcept { return !static_cast<std::underlying_type_t<E>>(arg); } \
+	static constexpr auto _const operator+(E arg) noexcept { return +static_cast<std::underlying_type_t<E>>(arg); } \
+	static constexpr E _const operator~(E arg) noexcept { return static_cast<E>(~static_cast<std::underlying_type_t<E>>(arg)); } \
+	static constexpr E _const operator&(E lhs, E rhs) noexcept { return static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) & static_cast<std::underlying_type_t<E>>(rhs)); } \
+	static constexpr E _const operator|(E lhs, E rhs) noexcept { return static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) | static_cast<std::underlying_type_t<E>>(rhs)); } \
+	static constexpr E _const operator^(E lhs, E rhs) noexcept { return static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) ^ static_cast<std::underlying_type_t<E>>(rhs)); } \
+	static constexpr E & operator&=(E &lhs, E rhs) noexcept { return lhs = static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) & static_cast<std::underlying_type_t<E>>(rhs)); } \
+	static constexpr E & operator|=(E &lhs, E rhs) noexcept { return lhs = static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) | static_cast<std::underlying_type_t<E>>(rhs)); } \
+	static constexpr E & operator^=(E &lhs, E rhs) noexcept { return lhs = static_cast<E>(static_cast<std::underlying_type_t<E>>(lhs) ^ static_cast<std::underlying_type_t<E>>(rhs)); } \
+	static inline std::ostream & operator<<(std::ostream &os, E e) { \
 		auto orig_flags = os.flags(std::ios_base::hex | std::ios_base::showbase); \
 		os << +static_cast<std::underlying_type_t<E>>(e); \
 		os.flags(orig_flags); \
